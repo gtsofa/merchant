541e5987e405d1752127b95ccccc337e08993c7c
gtsofa <jlsnyule@yahoo.com>
2017-04-22 01:12:27 +0300

    OrderItem Subtotal

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,4 +2,9 @@ class OrderItem < ApplicationRecord
   belongs_to  :Order
   belongs_to  :Product
   validates :order_id, :product_id, presence: true
+
+  def subtotal
+    return item.quantity * item.product.price
+    #quantity * product.price
+  end
 end
