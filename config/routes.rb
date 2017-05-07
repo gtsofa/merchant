ebdc3c010a533325c8b2bcc145f30bc8aa81033b
gtsofa <jlsnyule@yahoo.com>
2017-05-07 22:54:03 +0300

    creating a session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :order_items
   resources :products
+
+  match '/auth/:provider/callback', to: 'sessions#create', via: :get
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
