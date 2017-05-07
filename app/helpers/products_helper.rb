16554c5d347eaedc4b977f38b24f5fe44b5be5eb
gtsofa <jlsnyule@yahoo.com>
2017-05-07 22:21:34 +0300

    fixed a bug on print_stock intelligent

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -8,9 +8,10 @@ module ProductsHelper
     if stock == 0
         content_tag(:span, "Out of Stock", class: "out_stock")
       elsif stock >= requested
-        content_tag(:span, "In Stock #{stock}", class: "in_stock")
+        content_tag(:span, "In Stock (#{stock})", class: "in_stock")
+
       elsif stock < requested
-        content_tag(:span, "Insufficient stock #{stock}", class: "low_stock")
+        content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
 
     end
   end
