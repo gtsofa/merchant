7b1396f2d16a59788248828a43f913101f63f709
gtsofa <jlsnyule@yahoo.com>
2017-05-11 21:23:23 +0300

    refactored iteration 6

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :products, :through => :order_items
+  belongs_to :user
 
   def total
     total = order_items.inject(0) { |sum, p| sum + p.subtotal }
